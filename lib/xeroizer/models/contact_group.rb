048a906370a329c5bdd9c3e4cd36075dc2de0ea2
Marek Buszman <mbusiq@gmail.com>
2016-02-01 11:26:27 +0100

    allow adding multiple contacts to groups with one request

--- lib/xeroizer/models/contact_group.rb
+++ lib/xeroizer/models/contact_group.rb
@@ -13,6 +13,26 @@ module Xeroizer
 
       has_many :contacts, :list_complete => true
 
+      def add_contacts_by_ids(ids)
+        xml = contacts_xml(ids)
+        log "[UPDATE SENT] (#{__FILE__}:#{__LINE__}) \r\n#{xml}"
+        client = parent.application.client
+        url = [parent.url, id, 'Contacts'].join('/')
+        parent.application.http_put(client, url, xml, {})
+      end
+
+      private
+
+      def contacts_xml(ids)
+        b = Builder::XmlMarkup.new(indent: 2)
+        b.contacts do
+          ids.each do |contact_id|
+            b.contact do
+              b.tag! 'ContactID', contact_id
+            end
+          end
+        end
+      end
     end
 
   end
